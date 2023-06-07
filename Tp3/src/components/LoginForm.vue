d07e85d5f4106a3485b570b48859fd2bd16b66ef
Olivier Pouliot <sevenwanderings@gmail.com>
2023-06-07 10:49:44 -0400

    Html

---
+++ Tp3/src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <form>
+            <div class="form-group">
+                <label for="inputEmail">Email address</label>
+                <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We will never share your email..but your pass...</small>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword">Password</label>
+                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
