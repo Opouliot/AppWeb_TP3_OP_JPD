4ba60ba9cd5fd1a1264562e732bedccd2ffc1815
Olivier Pouliot <sevenwanderings@gmail.com>
2023-06-07 11:49:45 -0400

    html

---
+++ Tp3/src/components/DeleteMovie.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <label for="deleteMovie">Delete Movie?</label>
+        <button>Delete</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
