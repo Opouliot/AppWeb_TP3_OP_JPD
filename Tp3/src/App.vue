a21541c27aec7c9fa41e55538207f38d6b04360e
Olivier Pouliot <sevenwanderings@gmail.com>
2023-06-13 09:30:46 -0400

    gossage avec de bugs

--- Tp3/src/App.vue
+++ Tp3/src/App.vue
@@ -1,23 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 
-import AddMovieForm from './components/AddMovieForm.vue';
+import FilmDetailView from './views/FilmDetailView.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <!-- <AddMovieForm></AddMovieForm> -->
-
-      <nav>
-
-      </nav>
+      
+      <FilmDetailView/>
     </div>
   </header>
-
-
 </template>
 
 <style scoped>
