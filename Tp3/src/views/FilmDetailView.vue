e45284fc3ac036c499f82044b84e89faa65df39b
Jean-Philippe Delisle <delisle_jp@outlook.com>
2023-06-21 13:23:36 -0400

    bob est grand

--- Tp3/src/views/FilmDetailView.vue
+++ Tp3/src/views/FilmDetailView.vue
@@ -1,12 +1,29 @@
-<script setup>
-    import WelcomeWord from '../components/WelcomeWord.vue'
-</script>
-
 <template>
-    <main>
-        <WelcomeWord></WelcomeWord>
-    </main>
+    <MovieDetail ></MovieDetail> 
 </template>
+
+<script>
+    import { ref } from 'vue'
+    import { storeToRefs } from 'pinia'
+    import router from '@/router/index.js'
+
+    import { useUserStore } from '@/stores/userStore.js'
+    import MovieDetail from '@/components/MovieDetail.vue';
+
+    export default {
+        props: {
+            id: String,
+        },
+        components: {
+            MovieDetail
+        },
+        setup(props) {
+            const userStore = useUserStore();
+            const { isConnected, isAdmin } = storeToRefs(userStore);
+
+        }
+    }
+</script>
 
 <style scoped>
 
